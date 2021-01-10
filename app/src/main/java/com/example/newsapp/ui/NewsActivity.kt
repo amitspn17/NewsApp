9c2b79878d6e3b6f8a64a5af1a4f940e75fb969b
Amit Singh <amitsingh@singsys.com>
2021-01-10 16:41:55 +0530

    first commit

---
+++ app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
@@ -0,0 +1,25 @@
+package com.example.newsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsapp.R
+import com.example.newsapp.db.ArticleDatabase
+import com.example.newsapp.repository.NewsRepository
+import kotlinx.android.synthetic.main.activity_news.*
+
+class NewsActivity : AppCompatActivity() {
+
+    lateinit var  viewModel: NewsViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+        val  newsrepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsrepository)
+        viewModel = ViewModelProvider(this,viewModelProviderFactory).get(NewsViewModel::class.java)
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/util/Constant.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.util
+
+class Constant {
+    companion object{
+        const val API_KEY="7b9c0f75043d4ecb95e7dc9eac0a6f16"
+        const val BASE_URL ="http://newsApi.org"
+        const val  SEARCH_NEWS_TIME_DELAY = 500L
+        const val  QUERY_PAGE_SIZE = 20
+    }
+}
